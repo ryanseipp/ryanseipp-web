c043124971ceefa3c60588e548b13ec8feb24bdd
Ryan Seipp <rseipp@ryanseipp.com>
2023-12-06 20:27:38 -0500

    Add fonts, update maxWidth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        prose: "80ch",
+      },
+      fontFamily: {
+        sans: ['"Source Sans 3 Variable"', ...defaultTheme.fontFamily.sans],
+        mono: ["Source Code Pro Variable", ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
