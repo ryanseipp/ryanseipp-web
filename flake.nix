df2355cbe5ba1da7fde167b7e693ac6b98690457
Ryan Seipp <rseipp@ryanseipp.com>
2024-06-08 09:44:09 -0400

    feat: add nix flake, update to astro 4.10

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Everything behind the ryanseipp.com domain";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells.default = pkgs.mkShell {
+        packages = (with pkgs; [nodejs_20]) ++ (with pkgs.nodePackages; [pnpm prettier typescript]);
+      };
+    });
+}
